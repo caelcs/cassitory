5c00d1c156316a79056b35a310eed045d056a7b4
cael <adolfoecs@gmail.com>
2019-06-05 08:24:04 +0100

    adding more test scenarios

--- src/test/java/uk/co/caeldev/cassitory/repository/BaseRepositoryTest.java
+++ src/test/java/uk/co/caeldev/cassitory/repository/BaseRepositoryTest.java
@@ -39,10 +39,10 @@ class BaseRepositoryTest {
     private Session session;
 
     @Mock
-    private Statement saveQuery;
+    private Statement deleteQuery;
 
     @Mock
-    private Statement saveQueryByName;
+    private Statement deleteQueryByName;
 
     @Captor
     private ArgumentCaptor<BatchStatement> batchStatementArgumentCaptor;
@@ -60,8 +60,8 @@ class BaseRepositoryTest {
         when(mappingManager.mapper(UserByName.class)).thenReturn(mapperByName);
 
         //And
-        when(mapper.saveQuery(any(User.class), any(Mapper.Option.class))).thenReturn(saveQuery);
-        when(mapperByName.saveQuery(any(UserByName.class), any(Mapper.Option.class))).thenReturn(saveQueryByName);
+        when(mapper.saveQuery(any(User.class), any(Mapper.Option.class))).thenReturn(deleteQuery);
+        when(mapperByName.saveQuery(any(UserByName.class), any(Mapper.Option.class))).thenReturn(deleteQueryByName);
 
         //And
         when(mappingManager.getSession()).thenReturn(session);
@@ -71,12 +71,111 @@ class BaseRepositoryTest {
         userDtoRepository.save(userDto);
 
         //Then
-        verify(mapper).saveQuery(any(User.class), any(Mapper.Option.class));
-        verify(mapperByName).saveQuery(any(UserByName.class), any(Mapper.Option.class));
-
         verify(session).execute(batchStatementArgumentCaptor.capture());
         BatchStatement batchStatement = batchStatementArgumentCaptor.getValue();
-        assertThat(batchStatement.getStatements()).containsExactlyInAnyOrder(saveQuery, saveQueryByName);
+        assertThat(batchStatement.getStatements()).containsExactlyInAnyOrder(deleteQuery, deleteQueryByName);
+
+        assertThat(userDtoRepository.mappers).containsOnlyKeys(User.class, UserByName.class);
+        assertThat(userDtoRepository.mappers).containsEntry(User.class, mapper);
+        assertThat(userDtoRepository.mappers).containsEntry(UserByName.class, mapperByName);
+        assertThat(userDtoRepository.getTargetClasses()).containsExactly(User.class, UserByName.class);
+    }
+
+    @Test
+    @DisplayName("Should delete a Dto from User and UserByName table")
+    public void shouldDeleteFromUserAndUserByNameTables() {
+        //Given
+        UserDto userDto = new UserDto();
+        userDto.setName(string().next());
+        userDto.setAge(integer().next());
+
+        //And
+        when(mappingManager.mapper(User.class)).thenReturn(mapper);
+        when(mappingManager.mapper(UserByName.class)).thenReturn(mapperByName);
+
+        //And
+        when(mapper.deleteQuery(any(User.class), any(Mapper.Option.class))).thenReturn(deleteQuery);
+        when(mapperByName.deleteQuery(any(UserByName.class), any(Mapper.Option.class))).thenReturn(deleteQueryByName);
+
+        //And
+        when(mappingManager.getSession()).thenReturn(session);
+
+        //When
+        UserDtoRepository userDtoRepository = new UserDtoRepository(mappingManager);
+        userDtoRepository.delete(userDto);
+
+        //Then
+        verify(session).execute(batchStatementArgumentCaptor.capture());
+        BatchStatement batchStatement = batchStatementArgumentCaptor.getValue();
+        assertThat(batchStatement.getStatements()).containsExactlyInAnyOrder(deleteQuery, deleteQueryByName);
+
+        assertThat(userDtoRepository.mappers).containsOnlyKeys(User.class, UserByName.class);
+        assertThat(userDtoRepository.mappers).containsEntry(User.class, mapper);
+        assertThat(userDtoRepository.mappers).containsEntry(UserByName.class, mapperByName);
+        assertThat(userDtoRepository.getTargetClasses()).containsExactly(User.class, UserByName.class);
+    }
+
+    @Test
+    @DisplayName("Should save async a Dto to User and UserByName table")
+    public void shouldSaveAsyncToUserAndUserByNameTables() {
+        //Given
+        UserDto userDto = new UserDto();
+        userDto.setName(string().next());
+        userDto.setAge(integer().next());
+
+        //And
+        when(mappingManager.mapper(User.class)).thenReturn(mapper);
+        when(mappingManager.mapper(UserByName.class)).thenReturn(mapperByName);
+
+        //And
+        when(mapper.saveQuery(any(User.class), any(Mapper.Option.class))).thenReturn(deleteQuery);
+        when(mapperByName.saveQuery(any(UserByName.class), any(Mapper.Option.class))).thenReturn(deleteQueryByName);
+
+        //And
+        when(mappingManager.getSession()).thenReturn(session);
+
+        //When
+        UserDtoRepository userDtoRepository = new UserDtoRepository(mappingManager);
+        userDtoRepository.saveAsync(userDto);
+
+        //Then
+        verify(session).executeAsync(batchStatementArgumentCaptor.capture());
+        BatchStatement batchStatement = batchStatementArgumentCaptor.getValue();
+        assertThat(batchStatement.getStatements()).containsExactlyInAnyOrder(deleteQuery, deleteQueryByName);
+
+        assertThat(userDtoRepository.mappers).containsOnlyKeys(User.class, UserByName.class);
+        assertThat(userDtoRepository.mappers).containsEntry(User.class, mapper);
+        assertThat(userDtoRepository.mappers).containsEntry(UserByName.class, mapperByName);
+        assertThat(userDtoRepository.getTargetClasses()).containsExactly(User.class, UserByName.class);
+    }
+
+    @Test
+    @DisplayName("Should delete async a Dto from User and UserByName table")
+    public void shouldDeleteAsyncFromUserAndUserByNameTables() {
+        //Given
+        UserDto userDto = new UserDto();
+        userDto.setName(string().next());
+        userDto.setAge(integer().next());
+
+        //And
+        when(mappingManager.mapper(User.class)).thenReturn(mapper);
+        when(mappingManager.mapper(UserByName.class)).thenReturn(mapperByName);
+
+        //And
+        when(mapper.deleteQuery(any(User.class), any(Mapper.Option.class))).thenReturn(deleteQuery);
+        when(mapperByName.deleteQuery(any(UserByName.class), any(Mapper.Option.class))).thenReturn(deleteQueryByName);
+
+        //And
+        when(mappingManager.getSession()).thenReturn(session);
+
+        //When
+        UserDtoRepository userDtoRepository = new UserDtoRepository(mappingManager);
+        userDtoRepository.deleteAsync(userDto);
+
+        //Then
+        verify(session).executeAsync(batchStatementArgumentCaptor.capture());
+        BatchStatement batchStatement = batchStatementArgumentCaptor.getValue();
+        assertThat(batchStatement.getStatements()).containsExactlyInAnyOrder(deleteQuery, deleteQueryByName);
 
         assertThat(userDtoRepository.mappers).containsOnlyKeys(User.class, UserByName.class);
         assertThat(userDtoRepository.mappers).containsEntry(User.class, mapper);
